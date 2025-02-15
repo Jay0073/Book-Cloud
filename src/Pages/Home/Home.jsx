59506fa8eb5592d9b50ef1e450ff72cb1a5be653
Jayendra <voutlajay8765@gmail.com>
2025-02-16 00:53:04 +0530

    created components and pages

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../Components/Navbar/Navbar'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar />
+      this is home
+    </div>
+  )
+}
+
+export default Home
